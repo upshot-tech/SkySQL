db96857cfd7819ad160257a2fe9f79a0b2607deb
DaWe <willteam35@gmail.com>
2021-01-20 22:49:45 +0100

    reorg write file code block

--- generator/generate.js
+++ generator/generate.js
@@ -1,8 +1,7 @@
-const { initDataFolder, sortByKey } = require('./utils');
+const { initDataFolder, sortByKey, writeData } = require('./utils');
 fs = require('fs');
 const config = require('config');
-const noop = () => {};
-var dir = __dirname + '/../dist';
+
 
 initDataFolder()
 
@@ -21,43 +20,11 @@ for tables:
  */
 
 
-
 // Sort given data
 console.log('Sorting', data.length, 'rows')
 data.sort(sortByKey);
 
-// write data to files
-console.log('writing', parseInt(data.length/1000), 'files')
-var indexFileContent = 'index #\n'
-var linesInFile = 0
-var contentToWrite = 'final #\n'
-var nextFileName = 0
-var firstLineInFile = ''
-
-for (let i = 0; i < data.length; i++) {
-	if (linesInFile < 1000) {
-		contentToWrite += data[i][0] + ' ' + data[i][1] + '\n'
-		if (linesInFile == 0) {
-			firstLineInFile = data[i][0]
-		}
-		linesInFile += 1
-	} else {
-		fs.writeFileSync(dir + '/data/' + nextFileName + '.txt', contentToWrite, noop)
-		linesInFile = 0
-		contentToWrite = 'final #\n'
-		indexFileContent += firstLineInFile + ' data/' + nextFileName + '.txt\n'
-		firstLineInFile = ''
-		nextFileName += 1
-	}
-}
-if (linesInFile != 0) {
-	fs.writeFileSync(dir + '/data/' + nextFileName + '.txt', contentToWrite, noop)
-	indexFileContent += firstLineInFile + ' data/' + nextFileName + '.txt\n'
-}
-
-// WRITE indexFileContent
-console.log('writing index file')
-fs.writeFileSync(dir + '/data/index.txt', indexFileContent, noop)
+writeData(data, 'ddd')
 
 
 
--- generator/utils.js
+++ generator/utils.js
@@ -1,6 +1,9 @@
-touchDir = function touchDir(dir) {
-	if (!fs.existsSync(dir)){
-		fs.mkdirSync(dir);
+const dir = __dirname + '/../dist';
+const noop = () => {};
+
+touchDir = function touchDir(folder) {
+	if (!fs.existsSync(folder)){
+		fs.mkdirSync(folder);
 		return true
 	} else {
 		return false
@@ -8,13 +11,10 @@ touchDir = function touchDir(dir) {
 }
 
 exports.initDataFolder = function initDataFolder() {
-    var dir = __dirname + '/../dist';
     if (touchDir(dir) == false) {
         console.log("The /dist folder already exist, please delete it.")
         process.exit(1)
     }
-    
-    touchDir(dir + '/data')
     
     fs.copyFile( __dirname + '/../viewer/example.html', dir + '/index.html', (err) => {
         if (err) throw err;
@@ -35,4 +35,42 @@ exports.sortByKey = function sortByKey(x, y) {
         return 1;
     }
     return 0;
+}
+
+
+exports.writeData = function writeData(data, folder) {
+    touchDir(dir + '/' + folder)
+
+    // write data to files
+    console.log('writing', Math.ceil(data.length/1000), 'files')
+    var indexFileContent = 'index #\n'
+    var linesInFile = 0
+    var contentToWrite = 'final #\n'
+    var nextFileName = 0
+    var firstLineInFile = ''
+
+    for (let i = 0; i < data.length; i++) {
+        if (linesInFile < 1000) {
+            contentToWrite += data[i][0] + ' ' + data[i][1] + '\n'
+            if (linesInFile == 0) {
+                firstLineInFile = data[i][0]
+            }
+            linesInFile += 1
+        } else {
+            fs.writeFileSync(dir + '/' + folder + '/' + nextFileName + '.txt', contentToWrite, noop)
+            linesInFile = 0
+            contentToWrite = 'final #\n'
+            indexFileContent += firstLineInFile + ' ' + folder + '/' + nextFileName + '.txt\n'
+            firstLineInFile = ''
+            nextFileName += 1
+        }
+    }
+    if (linesInFile != 0) {
+        fs.writeFileSync(dir + '/' + folder + '/' + nextFileName + '.txt', contentToWrite, noop)
+        indexFileContent += firstLineInFile + ' ' + folder + '/' + nextFileName + '.txt\n'
+    }
+
+    // WRITE indexFileContent
+    console.log('writing index file')
+    fs.writeFileSync(dir + '/' + folder + '/index.txt', indexFileContent, noop)
 }
