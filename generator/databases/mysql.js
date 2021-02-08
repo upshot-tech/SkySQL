a26c6ec1d426c5fd2bd3bb1d16dd732f8562c614
DaWe <willteam35@gmail.com>
2021-02-08 11:05:25 +0100

    add comments

--- generator/databases/mysql.js
+++ generator/databases/mysql.js
@@ -20,18 +20,21 @@ class DB {
         this.connection.end();
     }
 
+    // returns the name of the primary column from a given table
     getPrimary(table, callback) {
         this.query("SHOW KEYS FROM " + table + " WHERE Key_name = 'PRIMARY'", function (result) {
             callback(result[0].Column_name)
         });
     }
 
+    // returns all tables from a database as an array ['table1', 'table2']
     getAllTables(database, callback) {
         this.query("SELECT table_name FROM information_schema.tables WHERE table_schema = ?", function (result) {
             callback(result)
         }, database);
     }
 
+    // returns all columns from a table as an array ['column1', 'column2']
     getAllColumns(table, callback) {
         this.query("show columns from " + table, function (result) {
             callback(result)
