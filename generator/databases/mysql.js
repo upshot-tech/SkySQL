6d931fb2b7126372cfc4e8fbca0110b774727279
DaWe <willteam35@gmail.com>
2021-02-28 21:27:59 +0100

    promisify mysql class

--- generator/databases/mysql.js
+++ generator/databases/mysql.js
@@ -9,11 +9,13 @@ class DB {
     }
 
     // call example: query("SELECT * FROM table WHERE aaa = :aaa AND bbb = :bbb",{ aaa: aaa, bbb: bbb })
-    query(query, callback, params = []) {
-        this.connection.query(query, params, function(error, results, fields){
-            if (error) throw error;
-            callback(results)
-        });
+    async query(query, params = []) {
+        return await new Promise((resolve, reject) => {
+            this.connection.query(query, params, function(error, results, fields){
+                if (error) throw error;
+                resolve(results)
+            })
+        })
     }
 
     end() {
@@ -21,44 +23,40 @@ class DB {
     }
 
     // returns the name of the primary column from a given table
-    getPrimary(table, callback) {
-        this.query("SHOW KEYS FROM " + table + " WHERE Key_name = 'PRIMARY'", function (result) {			
-			if (result.length==0) {
-				throw "No Primary key found!";
-			}
-			callback(result[0].Column_name)
-        });
+    async getPrimary(table) {
+        const result = await this.query("SHOW KEYS FROM " + table + " WHERE Key_name = 'PRIMARY'")
+        if (result.length==0) {
+            throw "No Primary key found!";
+        }
+        return result[0].Column_name
     }
 
     // returns all tables from a database as an array ['table1', 'table2']
-    getAllTables(callback) {
+    async getAllTables() {
         let database = config.get('dbConfig.database')
-        this.query("SELECT table_name FROM information_schema.tables WHERE table_schema = ?", function (result) {
-            var tables = [];
-			for(var i=0; i < result.length; i++){
-				tables.push(result[i].table_name);
-			}
-            callback(tables);
-        }, database);
+        const result = await this.query("SELECT table_name FROM information_schema.tables WHERE table_schema = ?", database)
+        var tables = []
+        for (var i=0; i < result.length; i++) {
+            tables.push(result[i].table_name)
+        }
+        return tables
     }
 
     // returns all columns from a table as an array ['column1', 'column2']
-    getAllColumns(tableName, callback) {
-        this.query("show columns from " + tableName, function (result) {
-			var columns = [];
-			for (var i=0; i < result.length; i++) {
-				columns.push(result[i].Field);
-			}
-            callback(columns);
-        });
+    async getAllColumns(tableName) {
+        const result = await this.query("show columns from " + tableName)
+        var columns = []
+        for (var i=0; i < result.length; i++) {
+            columns.push(result[i].Field)
+        }
+        return columns
     }
-	getAllIndex(tableName, callback) {
-		this.query(`select table_name, non_unique, group_concat(column_name order by seq_in_index) as index_columns 
-					from information_schema.statistics where table_schema not in ('information_schema', 'mysql', 'performance_schema', 'sys') 
-					AND TABLE_NAME=` + tableName + `GROUP BY table_name,non_unique")`, 
-			function (result) {
-				callback(result.index_columns);
-			})
+	
+    async getAllIndex(tableName) {
+        const result = await this.query(`select table_name, non_unique, group_concat(column_name order by seq_in_index) as index_columns 
+                from information_schema.statistics where table_schema not in ('information_schema', 'mysql', 'performance_schema', 'sys') 
+                AND TABLE_NAME=` + tableName + `GROUP BY table_name,non_unique")`)
+        return result.index_columns
 	}
 }
 
