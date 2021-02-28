0f1738e440c7739faf92aa781b7e28b541dbf59c
DaWe <willteam35@gmail.com>
2021-02-28 18:20:39 +0100

    async-wrapper

--- generator/generate.js
+++ generator/generate.js
@@ -8,46 +8,52 @@ const dbType = config.get('dbConfig.type')
 const { DB } = require('./databases/' + dbType)
 const db = new DB
 
-// remove dist folder 
-fs.rmdirSync(__dirname + '/../dist', { recursive: true });
 
-// make new dist folder with initial files
-initDataFolder()
+exports.generate = async function generate()  {
+	return await new Promise((resolve, reject) => {
+		// remove dist folder 
+		fs.rmdirSync(__dirname + '/../dist', { recursive: true });
 
+		// make new dist folder with initial files
+		initDataFolder()
 
-// I hate this callback hell, but class functions + mysql are not compatible with async/await. Took 2 hours to realize.
+		// I hate this callback hell, but class functions + mysql are not compatible with async/await. Took 2 hours to realize.
+		getTablesToExport(db, function(tables) {
+			// iterate on all tables
+			tables.forEach(table => {
+				console.log('Exporting "' + table.name + '" table')
+				// get primary indexed column
+				db.getPrimary(table.name, function(primaryIndex) {
+					console.log('primaryIndex:', primaryIndex)
+					
+					
+					// get all column names for export
+					getColsToExport(db, table, function(cols) {
+						// iterate on all tables
+						tableQueryName = initTableName(table.name)
+						db.query('SELECT ' + cols + ' FROM ' + tableQueryName, function(rawData) {
 
-getTablesToExport(db, function(tables) {
-	// iterate on all tables
-	tables.forEach(table => {
-		console.log('Exporting "' + table.name + '" table')
-		// get primary indexed column
-		db.getPrimary(table.name, function(primaryIndex) {
-			console.log('primaryIndex:', primaryIndex)
-			
-			
-			// get all column names for export
-			getColsToExport(db, table, function(cols) {
-				// iterate on all tables
-				tableQueryName = initTableName(table.name)
-				db.query('SELECT ' + cols + ' FROM ' + tableQueryName, function(rawData) {
-
-					data = stringifyData(rawData, primaryIndex)
-					console.log('Sorting', data.length, 'rows')
-					data.sort(sortByKey)
-					writeData(data, table.name, 'data')
-
-					getIndexesToExport(db, table, function(indexes) {
-						// export all indexes
-						indexes.forEach(index => {
-							data = stringifyIndex(rawData, primaryIndex, index)
+							data = stringifyData(rawData, primaryIndex)
 							console.log('Sorting', data.length, 'rows')
 							data.sort(sortByKey)
-							writeData(data, table.name, index)
+							writeData(data, table.name, 'data')
+
+							getIndexesToExport(db, table, function(indexes) {
+								// export all indexes
+								indexes.forEach(index => {
+									data = stringifyIndex(rawData, primaryIndex, index)
+									console.log('Sorting', data.length, 'rows')
+									data.sort(sortByKey)
+									writeData(data, table.name, index)
+								})
+								
+								db.end()
+								resolve(true)
+							})
 						})
 					})
 				})
 			})
 		})
 	})
-})
+}
