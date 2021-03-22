4e9dba91eb5492cdb113d243b77f0c3c5c4c07b9
DaWe <willteam35@gmail.com>
2021-03-22 12:42:41 +0100

    generate json file header

--- generator/utils.js
+++ generator/utils.js
@@ -51,9 +51,9 @@ function writeData(data, tablename, folder) {
     // write data to files
     // console.log('writing', Math.ceil(data.length/1000), 'files')
     if (folder == 'data') {
-        var contentToWrite = '{"fileType": "table", "dataType": "int", "divider": " "} #\n'
+        var contentToWrite = '{"fileType": "table", "orderType": "number", "divider": " "} #\n'
     } else {
-        var contentToWrite = 'column_index #\n'
+        var contentToWrite = '{"fileType": "column_index", "orderType": "number", "divider": " "} #\n'
     }
     var indexFileContent = 'index #\n'
     var linesInFile = 0
@@ -71,9 +71,9 @@ function writeData(data, tablename, folder) {
             fs.writeFileSync(writefolder + '/' + nextFileName + '.txt', contentToWrite, noop)
             linesInFile = 0
             if (folder == 'data') {
-                contentToWrite = 'table #\n'
+                contentToWrite = '{"fileType": "table", "orderType": "number", "divider": " "} #\n'
             } else {
-                contentToWrite = 'column_index #\n'
+                contentToWrite = '{"fileType": "column_index", "orderType": "number", "divider": " "} #\n'
             }
             indexFileContent += firstLineInFile + ' ' + nextFileName + '.txt\n'
             firstLineInFile = ''
